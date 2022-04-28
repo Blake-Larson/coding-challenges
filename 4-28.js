eaca808d0741472df69044b0691ecf63d6d49188
Blake Larson <Blarson224@gmail.com>
2022-04-28 16:07:16 -0400

    added code

---
+++ 4-28.js
@@ -0,0 +1,7 @@
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+function removeExclamationMarks(s) {
+    while (s.includes('!')){
+      s = s.replace('!', '') 
+    }
+    return s
+  }
