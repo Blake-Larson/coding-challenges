2ddfa1f0deb11e8c2fcefdf316984adb48fea0f6
Blake Larson <Blarson224@gmail.com>
2022-05-13 13:29:53 -0400

    solution036 complete

--- js/solution036.js
+++ js/solution036.js
@@ -1,14 +1,4 @@
 
-
-
-// Examples
-// "hello"     -->  "hll"
-// "codewars"  -->  "cdwrs"
-// "goodbye"   -->  "gdby"
-// "HELLO"     -->  "HELLO"
-// don't worry about uppercase vowels
-// y is not considered a vowel for this kata
-
 // Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
 
 //P: string, not including y, leave uppercase alone
