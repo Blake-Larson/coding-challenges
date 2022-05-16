702781ec352cbe22cbc291d159e1ca1bf1575959
Blake Larson <Blarson224@gmail.com>
2022-05-16 11:27:22 -0400

    solution043 complete

---
+++ js/solution043.js
@@ -0,0 +1,15 @@
+//take n and return n in binary format
+
+//P: number, non-negative, integer
+
+//R: return binary format, which is numbers
+
+function toBinary(n){
+    return +n.toString(2)
+}
+
+
+//E:
+
+toBinary(1) // 1
+toBinary(3) // 11
