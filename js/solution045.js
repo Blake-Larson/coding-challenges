b4f72191104f81c269e4f8e62316d392f3c62cf7
Blake Larson <Blarson224@gmail.com>
2022-05-17 11:21:37 -0400

    solution045 complete

---
+++ js/solution045.js
@@ -0,0 +1,17 @@
+// if x squared is more than 1000 return string
+
+//P: will be integer, not negative, could be string
+
+//R: return string based on true or false
+
+function apple(x){
+    //make x number
+    //if statement
+    Math.pow(Number(x),2) > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+
+
+//E:
+
+apple(5)// return false string
+apple(500)//return true string
