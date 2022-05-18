1f0ee59e3366bf98c328043f89fa9a947b9ad1ca
Blake Larson <Blarson224@gmail.com>
2022-05-18 11:03:20 -0400

    solution047 complete

---
+++ js/solution047.js
@@ -0,0 +1,10 @@
+
+
+//Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+
+//P: number, not negative
+
+//R: return number
+
+const twoDecimalPlaces = n =>  Number(n.toFixed(2))
