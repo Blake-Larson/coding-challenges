279ccb0f3e9eba494ddd8930d03d96e3f1af6a1b
Blake Larson <Blarson224@gmail.com>
2022-05-22 09:32:21 -0400

    solution053 complete

---
+++ js/solution053.js
@@ -0,0 +1,30 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+// Your task is to return one of the following strings:
+
+// < firstName here >, < country here > of the first Python developer who has signed up; or
+// There will be no Python developers if no Python developer has signed up.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+//   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+// ];
+// your function should return Victoria, Puerto Rico.
+
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+
+//P: array of peple objects
+
+//R: a string if there are python developers, first name, country else no python devs
+
+//filter for python and return name and country of that array element
+
+
+function getFirstPython(list) {
+    let pythonDev = list.find(person => person.language === 'Python')
+    return pythonDev === undefined ? 'There will be no Python developers' : `${pythonDev.firstName}, ${pythonDev.country}`
+}
