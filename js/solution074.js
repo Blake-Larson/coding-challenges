6560003504ec0bdb3d0033ca8b8fb5d09ba1d01f
Blake Larson <Blarson224@gmail.com>
2022-06-17 09:04:53 -0400

    solution074 complete

---
+++ js/solution074.js
@@ -0,0 +1,16 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//P: number, integer?, positive?
+
+//R: square of num
+
+function squareNum(num){
+return num * num
+}
+
+
+
+//E:
+
+squareNum(2) // 4
+squareNum(5) // 25
