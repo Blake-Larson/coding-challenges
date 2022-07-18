1115d1c0ae2bebefdb6a6edeb64cb27fb3b30952
Blake Larson <Blarson224@gmail.com>
2022-07-18 11:31:45 -0700

    solution106 complete

--- js/solution106.js
+++ js/solution106.js
@@ -42,5 +42,5 @@
 
   function removeSmallest(numbers) {
     let indexOfMin = numbers.indexOf(Math.min(...numbers));
-    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)]; 
   }
