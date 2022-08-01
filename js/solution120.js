cccfde1ed91981b6e2888ebc309be6fd3c227261
Blake Larson <Blarson224@gmail.com>
2022-08-01 09:47:55 -0700

    solution120 complete

---
+++ js/solution120.js
@@ -0,0 +1,13 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+	// could use sort then use conditional to return the right number
+	let sorted = arr.sort((a, b) => a - b);
+	sorted[0] !== sorted[1] ? sorted[0] : sorted[sorted.length - 1];
+}
