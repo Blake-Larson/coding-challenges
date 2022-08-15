dad920144ae0417dc05e0f14262f40cf6b062376
Blake Larson <Blarson224@gmail.com>
2022-08-15 08:07:10 -0700

    old changes

---
+++ js/solution093.js
@@ -0,0 +1,21 @@
+//define a book with n pages with pages 1 to n
+//define summary adding up the digits of all page numbers
+
+//given the summary find the number of pages the book has
+
+// a positive integer > 0
+
+// return number of pages
+
+// example:
+
+// 6 => 1,2,3,4,5,6
+// 11 => 1,2,3,4,5,6,7,8,9,10
+
+function getPages(){
+    // 9 or less same number of pages as summary
+
+    // 10 
+
+
+}
