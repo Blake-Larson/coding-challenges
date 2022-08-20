ed9676583ea49a4de5e93d157dd960b0fac295e1
Blake Larson <Blarson224@gmail.com>
2022-08-20 12:20:48 -0700

    solution 139 complete

--- js/solution139.js
+++ js/solution139.js
@@ -8,17 +8,7 @@
 //R: Boolean
 
 function isPalindrome(line) {
-	let lineArr = line.toString().split('');
-	if (lineArr.length % 2 !== 0) {
-		lineArr.splice(Math.floor(lineArr.length / 2), 1);
-	}
-	let first = lineArr
-		.slice(0, Math.floor(lineArr.length / 2))
-		.reverse()
-		.join('');
-	let second = lineArr.slice(lineArr.length / 2).join('');
-
-	return first === second;
+	console.log(String(line) == String(line).split('').reverse().join(''));
 }
 
 isPalindrome('anna'); // ==> true
