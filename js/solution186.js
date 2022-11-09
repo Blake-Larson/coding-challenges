1df55adca01681af7857f5c330d1d10ffb9f988f
Blake Larson <Blarson224@gmail.com>
2022-11-09 10:02:53 -0800

    solution186 complete

--- js/solution186.js
+++ js/solution186.js
@@ -4,24 +4,33 @@
 
 //return the number that was deleted
 
-function findDeletedNumber(arr, alteredArr) {
-	// sort the alteredArr
-	alteredArr.sort((a, b) => a - b);
+// function findDeletedNumber(arr, alteredArr) {
+// 	// sort the alteredArr
+// 	alteredArr.sort((a, b) => a - b);
 
-	//forEach to compare
-	return arr.find((el, i) => el !== alteredArr[i])
-		? arr.find((el, i) => el !== alteredArr[i])
-		: 0;
+// 	//forEach to compare
+// 	return arr.find((el, i) => el !== alteredArr[i])
+// 		? arr.find((el, i) => el !== alteredArr[i])
+// 		: 0;
+// }
+
+// console.log(findDeletedNumber([1, 2, 3, 4, 5], [3, 4, 1, 5]), 2, 'Deletion');
+// console.log(
+// 	findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 9, 7, 4, 6, 2, 3, 8]),
+// 	5,
+// 	'Deletion'
+// );
+// console.log(
+// 	findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [5, 7, 6, 9, 4, 8, 1, 2, 3]),
+// 	0,
+// 	'No deletion'
+// );
+function findDeletedNumber(arr, mixedArr) {
+	if (arr.length === mixedArr.length) {
+		return 0;
+	} else {
+		let arrSum = arr.reduce((acc, c) => acc + c, 0);
+		let mixedArrSum = mixedArr.reduce((acc, c) => acc + c, 0);
+		return arrSum - mixedArrSum;
+	}
 }
-
-console.log(findDeletedNumber([1, 2, 3, 4, 5], [3, 4, 1, 5]), 2, 'Deletion');
-console.log(
-	findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 9, 7, 4, 6, 2, 3, 8]),
-	5,
-	'Deletion'
-);
-console.log(
-	findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [5, 7, 6, 9, 4, 8, 1, 2, 3]),
-	0,
-	'No deletion'
-);
