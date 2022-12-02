caaf64e1a264fa6f4886d88f5835c5939dc3749b
Blake Larson <Blarson224@gmail.com>
2022-12-02 15:39:16 -0800

    solution206 complete

---
+++ js/solution206.js
@@ -0,0 +1,35 @@
+// Given a string of characters, return the character that appears the most often.
+
+//always a string, valid, could be spaces, always will be repeating characters, can be caps
+
+//return the character as a string
+
+// findMost('aabcde'), a
+// findMost('Audreyy'), y
+// findMost('PiZza'), z
+
+function findMost(str) {
+	//count how many times each character appears by using a hashmap
+	const map = {};
+	let strArr = str.toLowerCase().split('');
+
+	for (el of strArr) {
+		if (map[el]) {
+			map[el] += 1;
+		} else map[el] = 1;
+	}
+
+	let num = 0;
+	let letter = '';
+	for (el in map) {
+		if (map[el] > num) {
+			num = map[el];
+			letter = el;
+		}
+	}
+	return letter;
+}
+
+console.log(findMost('aabcde'), 'a');
+console.log(findMost('Audreyy'), 'y');
+console.log(findMost('PiZza'), 'z');
