ec1cb22369542730166e16754bb6f5491819c763
Blake Larson <Blarson224@gmail.com>
2022-12-30 08:44:31 -0800

    solution220 complete

---
+++ js/solution220.js
@@ -0,0 +1,21 @@
+//Two Sum: Given an array of numbers, return all pairs that add up to a given sum.
+
+function twoSum(arr, sum) {
+	const map = {};
+	let pairs = [];
+
+	for (let num1 of arr) {
+		let num2 = sum - num1;
+		if (map[num2]) {
+			pairs.push([num1, num2]);
+		} else {
+			map[num1] = 1;
+		}
+	}
+	return pairs;
+}
+
+console.log(twoSum([1, 2, 2, 3, 4], 4), [
+	[2, 2],
+	[3, 1],
+]);
