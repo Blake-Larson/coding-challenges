97edd49fd820db7f9f56ea816d6bdeebb9ec6779
Blake Larson <Blarson224@gmail.com>
2023-01-20 10:09:56 -0800

    solution233 complete

---
+++ js/solution233.js
@@ -0,0 +1,62 @@
+// Factorial
+
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+
+// 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+// The value of 0! is 1.
+
+// #Your task
+
+// You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+//non-negative integer, always valid number
+
+//return n! as a number
+
+//factorial(5), 120
+//factorial(6), 720
+
+//iterative solution, recursive solution, memoized solution
+//https://pythontutor.com/javascript.html#mode=edit
+
+//recursion
+function factorial(num) {
+	if (num < 2) {
+		return 1;
+	}
+	return num * factorial(num - 1);
+}
+
+//memoization
+
+let memo = {};
+function factorial(n) {
+	if (n < 2) {
+		return 1;
+	} else if (memo[n]) {
+		return memo[n];
+	} else {
+		let result = n * factorial(n - 1);
+		memo[n] = result;
+		return result;
+	}
+}
+
+//iterative
+
+function factorial(num) {
+	let result = 1;
+	for (let i = 1; i <= num; i++) {
+		result *= i;
+	}
+	return result;
+}
+
+console.log(factorial(5), 120);
+console.log(factorial(6), 720);
+
+// 5 * factorial(5 - 1)
+//   4 * factorial(4 - 1)
+//     3 * factorial(3 - 1)
+//       2 * factorial(2 - 1)
